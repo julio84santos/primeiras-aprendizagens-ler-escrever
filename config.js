0a9a7dee24d38a577018e9cf2f67991cb6e75f58
julio84santos <julio84santos@gmail.com>
2026-07-30 22:15:23 -0300

    Adiciona links da Hotmart

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-// Cole os endereços dos checkouts da Hotmart entre as aspas.
 window.EDURECORTA_OFFERS = {
-  lerEscrever: "",
-  pensarContar: "",
-  coresFormasMedidas: "",
-  colecaoCompleta: ""
+  lerEscrever: "https://pay.hotmart.com/H106949194B ",
+  pensarContar: "https://pay.hotmart.com/SEU-LINK-PENSAR-CONTAR",
+  coresFormasMedidas: "https://pay.hotmart.com/SEU-LINK-CORES-FORMAS-MEDIDAS",
+  colecaoCompleta: "https://pay.hotmart.com/SEU-LINK-COMBO"
 };
+
+
+
+
+
