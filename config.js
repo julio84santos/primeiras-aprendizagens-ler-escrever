0d7dcafe82cd0e0e570de6bb7f55d0eabc58d1c1
julio84santos <julio84santos@gmail.com>
2026-07-30 22:24:33 -0300

    Adiciona links da Hotmart

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.EDURECORTA_OFFERS = {
   lerEscrever: "https://pay.hotmart.com/H106949194B ",
-  pensarContar: "https://pay.hotmart.com/SEU-LINK-PENSAR-CONTAR",
-  coresFormasMedidas: "https://pay.hotmart.com/SEU-LINK-CORES-FORMAS-MEDIDAS",
-  colecaoCompleta: "https://pay.hotmart.com/SEU-LINK-COMBO"
+  pensarContar: "https://pay.hotmart.com/F106952986X",
+  coresFormasMedidas: "https://pay.hotmart.com/A106953259Q",
+  colecaoCompleta: "https://pay.hotmart.com/Q106954985U"
 };
 
 
